0041c72690f2b6619bf869e86ff4543989a6b453
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-04-01 22:23:31 +0200

    02 some tests

--- 02/src/main.cpp
+++ 02/src/main.cpp
@@ -7,10 +7,22 @@
 
 int main() {
   char buffer[256] = {0};
+  char small_buffer[32];
   int ret = Printf(buffer, buffer+256,
                   "verbatim text %d, %u, %c, %s, %x, %b, %%",
                    -4316, 2958, '&', "some string", 0xdeadbeef, 0xdeadbeef)
                    ? 0 : 1;
   DEBUG(buffer);
+  ret |= Printf(small_buffer, small_buffer+32,
+                "%%d%%%d",
+                -123)
+                ? 0 : 1;
+  DEBUG(small_buffer);
+  /* should fail, ret swapped */
+  ret |= Printf(small_buffer, small_buffer+32,
+                "%s",
+                "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                ? 1 : 0;
+  DEBUG(small_buffer);
   return ret;
 }
